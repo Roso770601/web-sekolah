276bfcd8670bcddfa6e49f20831e51d1a9f2162e
Roso <jawadwipa2345@gmail.com>
2026-03-07 09:26:45 +0700

    Update nilai siswa otomatis

--- convert_excel_to_json.py
+++ convert_excel_to_json.py
@@ -1,29 +1,26 @@
 import pandas as pd
 import json
+import math
 
 df = pd.read_excel("nilai/data_siswa.xlsx")
-
-# GANTI SEMUA NaN MENJADI STRING KOSONG
-df = df.fillna("")
 
 data_list = []
 
 for _, row in df.iterrows():
-    siswa = {
-        "nisn": str(row["nisn"]).strip(),
-        "nama": str(row["nama"]).strip(),
-        "tgl_lahir": str(row["tgl_lahir"]).strip(),
-        "matematika": row["matematika"],
-        "bahasa_indonesia": row["bahasa_indonesia"],
-        "ppkn": row["ppkn"],
-        "ipas": row["ipas"],
-        "pjok": row["pjok"],
-        "seni_rupa": row["seni_rupa"]
-    }
+    siswa = {}
+
+    for kolom in df.columns:
+        nilai = row[kolom]
+
+        # Jika nilai NaN ubah menjadi string kosong
+        if pd.isna(nilai) or (isinstance(nilai, float) and math.isnan(nilai)):
+            nilai = ""
+
+        siswa[kolom] = nilai
 
     data_list.append(siswa)
 
 with open("data.json", "w", encoding="utf-8") as f:
     json.dump(data_list, f, indent=2, ensure_ascii=False)
 
-print("Excel berhasil diubah menjadi JSON")
+print("Excel berhasil diubah menjadi JSON tanpa NaN")
