67d51c787f99c6f931d144298a84119e94f89fdc
Roso <jawadwipa2345@gmail.com>
2026-03-07 09:33:41 +0700

    Update nilai siswa otomatis

--- convert_excel_to_json.py
+++ convert_excel_to_json.py
@@ -3,12 +3,13 @@ import json
 
 df = pd.read_excel("nilai/data_siswa.xlsx")
 
-# paksa semua NaN jadi kosong
+# ganti semua NaN dengan kosong
 df = df.fillna("")
 
+# ubah ke list json
 data_list = df.to_dict(orient="records")
 
 with open("data.json", "w", encoding="utf-8") as f:
-    json.dump(data_list, f, indent=2, ensure_ascii=False)
+    json.dump(data_list, f, indent=2, ensure_ascii=False, allow_nan=False)
 
 print("Excel berhasil diubah menjadi JSON tanpa NaN")
