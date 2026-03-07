98a92cf03204dd37e650d177543ee1dfec79e1ad
Roso <jawadwipa2345@gmail.com>
2026-03-07 09:48:07 +0700

    Update nilai siswa otomatis

--- convert_excel_to_json.py
+++ convert_excel_to_json.py
@@ -1,17 +1,12 @@
 import pandas as pd
 import json
 
-print("Membaca Excel...")
-
 df = pd.read_excel("nilai/data_siswa.xlsx")
 
-# ubah semua NaN menjadi string kosong
-df = df.fillna("")
+# ubah NaN menjadi None (akan jadi null di JSON)
+df = df.where(pd.notnull(df), None)
 
-# ubah ke list JSON
 data_list = df.to_dict(orient="records")
-
-print("Menyimpan JSON...")
 
 with open("data.json", "w", encoding="utf-8") as f:
     json.dump(data_list, f, indent=2, ensure_ascii=False)
