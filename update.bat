a1c4e7680baa024aef93c1f9f901b8c1905a630b
Roso <jawadwipa2345@gmail.com>
2026-03-08 14:34:16 +0700

    tambah halaman profil guru

--- update.bat
+++ update.bat
@@ -4,6 +4,12 @@ echo UPDATE NILAI WEBSITE
 echo =========================
 
 REM =========================
+REM AMBIL UPDATE TERBARU DARI GITHUB
+REM =========================
+echo Sinkronisasi dengan GitHub...
+git pull origin main
+
+REM =========================
 REM KONVERSI EXCEL KE JSON
 REM =========================
 echo Mengubah Excel ke JSON...
@@ -18,8 +24,9 @@ IF %ERRORLEVEL% NEQ 0 (
 REM =========================
 REM PUSH KE GITHUB
 REM =========================
-echo Menambahkan perubahan ke GitHub...
-git add .
+echo Mengirim update nilai ke GitHub...
+
+git add nilai/data.json
 git commit -m "Update nilai siswa otomatis"
 git push origin main
 
